82e00ae7f074b15637528ccdcb2491190da6e7b0
22T1020493 <115055341+22T1020493@users.noreply.github.com>
2022-12-04 16:53:26 +0700

    Add files via upload

--- FILENGUON.cpp
+++ FILENGUON.cpp
@@ -1,19 +1,34 @@
-// Doi so giay ra dong ho
-#include<bits/stdc++.h>
+//He phuong trinh bac nhat 2 an
+#include <bits/stdc++.h>
 using namespace std;
-int main()
-{
-int n; cin>>n;
-int temp, h,m,s;
-s=n%60;
-temp=n/60;
-m=temp%60;
-h=temp/60;
-if(h/10==0) cout<<"0"<<h<<":";
-else cout<<h<<":";
-if(m/10==0) cout<<"0"<<m<<":";
-else cout<<m<<":";
-if(s/10==0) cout<<"0"<<m;
-else cout<<s<<endl;
-
+int main (){
+	int n;
+	cin >> n;
+	int a[n];
+	for (int i=0;i<n;i++){
+		double a1,b1,c1,a2,b2,c2;
+		cin >> a1>>b1>>c1>>a2>>b2>>c2;
+		double D,Dx,Dy;
+		D = a1*b2-a2*b1;
+		Dx=c1*b2-c2*b1;
+		Dy=a1*c2-a2*c1;
+		double x,y;
+		if (D==0){
+			if (Dx+Dy==0){
+				cout << "Many solutions"<<endl;
+			}
+			else{
+				cout << "No solution"<<endl;
+			}
+		}
+		else {
+			x = Dx/D;
+			y = Dy/D;
+			cout <<setprecision(6)<<fixed<<x<<" ";
+			cout <<setprecision(6)<<fixed<<y;
+			cout << endl;
+		}
+	
+	}
+	return 0;
 }
